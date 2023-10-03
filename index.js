3b6f3a7eabdca5118235ace0594db1b71bdc5742
Rapha2202~ <raphael.foulonbinet@gmail.com>
2023-10-03 10:29:51 +0200

    Workshop

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk")
+
+console.log(chalk.blue("Salut Baptiste !"))
+console.log(chalk.red("Salut Doriane !"))
+console.log(chalk.green("Salut Jonathan !"))
+console.log(chalk.yellow("Salut Tristan !"))
